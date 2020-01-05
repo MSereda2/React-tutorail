9a804630c395e0258c49c856914d7f7f5e47182f
Misha Sereda <sereda2510@mail.ru>
2020-01-05 04:50:41 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import  React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends Component {
+    
+
+    render() {
+        return (
+            <h1>I love life</h1>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
